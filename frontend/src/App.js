8dcb0ab8278229cbef8eca13f991d4832e24f2c5
Sultan Karabekov <sulik23@gmail.com>
2021-01-07 21:53:06 -0500

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -72,7 +72,7 @@ function App() {
               )}
             ></Route>
           </div>
-          <div>
+          <div className="dropdown">
             <Link to="/cart">
               Cart
               {cartItems.length > 0 && (
